d6bd4999562ff19b43c6a0c8d8cf730c5e80345b
Cody Poll <CJPoll@gmail.com>
2013-09-18 23:46:11 -0600

    initial commit

---
+++ recipes/default.rb
@@ -0,0 +1,10 @@
+#
+# Cookbook Name:: vim
+# Recipe:: default
+#
+# Copyright 2013, Cody Poll
+#
+# All rights reserved - Do Not Redistribute
+#
+
+package 'vim'
---
+++ recipes/source.rb
@@ -0,0 +1,11 @@
+#
+# Cookbook Name:: vim
+# Recipe:: source
+#
+# Copyright 2013, Cody Poll
+#
+# All rights reserved - Do Not Redistribute
+#
+
+version = node[:version]
+puts version
